b42430ba0852354925e5cf1f1c05fcb4b8b42ff9
DanielFernando <143455420+DanielFernando45@users.noreply.github.com>
2025-08-01 10:27:04 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({  
-  origin: 'https://intranet.alejandriaconsultora.com', // o el host correcto
+  origin:[
+    'https://intranet.alejandriaconsultora.com', 
+    'https://intranet-alejandria-dnzgf89d3-danielfernandos-projects.vercel.app/',
+  ], // o el host correcto
   credentials: true,})
   app.useGlobalPipes(
     new ValidationPipe({
