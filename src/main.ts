11b66fd0c19ccf43b4ebcf4c5f55df8459417895
Calebcc <calebcondor553@gmail.com>
2025-10-03 14:46:11 -0500

    port

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,8 @@ async function bootstrap() {
       'https://intranet.alejandriaconsultora.com',
       'http://localhost:5174',
       'http://137.184.97.175:3001',
-      'http://backend:3000'
-
+      'http://backend:3000',
+      'https://intranet.alejandriaconsultora.com:3001',
     ],
     methods: 'GET, POST, PUT, DELETE, OPTIONS, PATCH', // Métodos HTTP permitidos
     allowedHeaders: 'Content-Type, Authorization, X-Requested-With', // Encabezados permitidos
