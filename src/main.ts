20f5a8e2c5ec70292256c312d5cbe250c08f78ff
Calebcc <calebcondor553@gmail.com>
2025-10-03 14:39:18 -0500

    Subiendo cambios

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,20 @@ async function bootstrap() {
 
   // const app = await NestFactory.create(AppModule, { httpsOptions });
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
-    origin: [`${process.env.FRONT_PORT_ENV}`],
-    credentials: true,
+    origin: [
+      `${process.env.FRONT_PORT_ENV}`,
+      'https://intranet.alejandriaconsultora.com',
+      'http://localhost:5174',
+      'http://137.184.97.175:3001',
+      'http://backend:3000'
+
+    ],
+    methods: 'GET, POST, PUT, DELETE, OPTIONS, PATCH', // Métodos HTTP permitidos
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With', // Encabezados permitidos
+    credentials: true, // Permitir credenciales como cookies
+    preflightContinue: false, // Deja que NestJS maneje la respuesta de preflight automáticamente
+    optionsSuccessStatus: 204, // Responder con estado 204 para solicitudes preflight
   });
 
   app.useGlobalPipes(
